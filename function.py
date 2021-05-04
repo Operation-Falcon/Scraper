c4900d7383c1752ea1866418176ae1ce2f622461
Operation Falcon <83413793+operationfalcon@users.noreply.github.com>
2021-05-04 20:05:38 +0530

    Update function.py

--- function.py
+++ function.py
@@ -15,15 +15,3 @@ def scrape_links_from_single_url(url, filename):
         print("Max retries exceeded with url. Failed to establish a new connection")
 
 
-def scrape_links_from_multiple_urls(filename, output):
-    try:
-        with open(filename, 'r')as file1:
-            file1=file1.readlines()
-            for url in file1:
-                web=requests.get(url).text
-                soup=BeautifulSoup(web, 'lxml')
-                with open(output, 'a') as file2:
-                    for link in soup.findAll('a'):
-                        file2.writelines('%s\n' % link.get('href'))
-    except Exception as e:
-        print("Max retries exceeded with url. Failed to establish a new connection")
